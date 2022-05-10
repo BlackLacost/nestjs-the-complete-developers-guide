7d01a1d66dcebfaf6cca873e0015e1b0d6276fb5
Ilya Lisin <blacklacost@gmail.com>
2022-05-10 23:06:00 +0300

    04 Validating Request Data with Pipes

--- 03 messages/src/messages/messages.controller.ts
+++ 03 messages/src/messages/messages.controller.ts
@@ -1,13 +1,23 @@
-import { Controller, Get, Post } from '@nestjs/common'
+import { Body, Controller, Get, Param, Post } from '@nestjs/common'
+import { CreateMessageDto } from './dto/create-message.dto'
+
+const messages: CreateMessageDto[] = []
 
 @Controller('messages')
 export class MessagesController {
   @Get()
-  all() {}
+  all() {
+    return messages
+  }
 
   @Post()
-  create() {}
+  create(@Body() input: CreateMessageDto) {
+    messages.push(input)
+    return input
+  }
 
   @Get(':id')
-  one() {}
+  one(@Param('id') id: number) {
+    return messages[id]
+  }
 }
