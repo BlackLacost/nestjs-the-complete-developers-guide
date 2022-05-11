c837a36db4275bbd4dd802bdc9981ee51de2816d
Ilya Lisin <blacklacost@gmail.com>
2022-05-11 16:02:14 +0300

    09 Creating and Saving User Data

--- 07 car-value/src/users/users.controller.ts
+++ 07 car-value/src/users/users.controller.ts
@@ -1,10 +1,44 @@
-import { Body, Controller, Post } from '@nestjs/common'
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common'
 import { CreateUserDto } from './dto/create-user.dto'
+import { UpdateUserDto } from './dto/update-user.dto'
+import { UsersService } from './users.service'
 
 @Controller('auth')
 export class UsersController {
+  constructor(private usersService: UsersService) {}
+
   @Post('signup')
   createUser(@Body() createUserDto: CreateUserDto) {
-    return createUserDto
+    const { email, password } = createUserDto
+    return this.usersService.create(email, password)
+  }
+
+  @Get(':id')
+  findUser(@Param('id') id: string) {
+    return this.usersService.findOne(parseInt(id))
+  }
+
+  @Get()
+  findAllUsers(@Query('email') email: string) {
+    return this.usersService.find(email)
+  }
+
+  @Patch(':id')
+  updateUser(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.usersService.update(parseInt(id), updateUserDto)
+  }
+
+  @Delete(':id')
+  removeUser(@Param('id') id: string) {
+    return this.usersService.remove(parseInt(id))
   }
 }
--- 07 car-value/src/users/users.service.ts
+++ 07 car-value/src/users/users.service.ts
@@ -1,4 +1,44 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, NotFoundException } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Repository } from 'typeorm'
+import { UpdateUserDto } from './dto/update-user.dto'
+import { User } from './user.entity'
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
+
+  create(email: string, password: string) {
+    const user = this.userRepository.create({ email, password })
+    return this.userRepository.save(user)
+  }
+
+  async findOne(id: number) {
+    const user = await this.userRepository.findOne(id)
+    if (!user) {
+      throw new NotFoundException(`User with id ${id} not found`)
+    }
+    return user
+  }
+
+  async find(email: string) {
+    const users = await this.userRepository.find({ email })
+    if (users.length === 0) {
+      throw new NotFoundException(`Users with email: ${email} not found`)
+    }
+    return users
+  }
+
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    const user = await this.findOne(id)
+    Object.assign(user, updateUserDto)
+    return this.userRepository.save(user)
+  }
+
+  async remove(id: number) {
+    const user = await this.findOne(id)
+    return this.userRepository.remove(user)
+  }
+}
