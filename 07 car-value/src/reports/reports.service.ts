0512dcd617b44b24f35674b5a7541116afde2f29
Ilya Lisin <blacklacost@gmail.com>
2022-05-14 11:13:07 +0300

    17 Query Builders with TypeORM

--- 07 car-value/src/reports/reports.service.ts
+++ 07 car-value/src/reports/reports.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm'
 import { Repository } from 'typeorm'
 import { User } from '../users/user.entity'
 import { CreateReportDto } from './dto/create-report.dto'
+import { GetEstimateDto } from './dto/get-estimate.dto'
 import { Report } from './report.entity'
 
 @Injectable()
@@ -25,5 +26,21 @@ export class ReportsService {
     }
     report.approved = approved
     return this.reportRepository.save(report)
+  }
+
+  createEstimate({ make, model, lng, lat, year, mileage }: GetEstimateDto) {
+    return this.reportRepository
+      .createQueryBuilder()
+      .select('AVG(price)', 'price')
+      .where('make = :make', { make })
+      .andWhere('model = :model', { model })
+      .andWhere('lng - :lng BETWEEN -5 AND 5', { lng })
+      .andWhere('lat - :lat BETWEEN -5 AND 5', { lat })
+      .andWhere('year - :year BETWEEN -3 AND 3', { year })
+      .andWhere('approved IS TRUE')
+      .orderBy('ABS(mileage - :mileage)', 'DESC')
+      .setParameters({ mileage })
+      .limit(3)
+      .getRawOne()
   }
 }
